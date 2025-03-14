c50d089cc5bab77c873271ca7816a0491a71d9ee
Buddhi Ridmal <buddhiridmal@gmail.com>
2025-03-14 14:41:04 +0530

    If the command is not a Get create the necessary response bhead and body , then flush through the output stream

--- src/main/java/webserver/ServerApp.java
+++ src/main/java/webserver/ServerApp.java
@@ -1,11 +1,9 @@
 package webserver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
 
 public class ServerApp {
     public static void main(String[] args) throws IOException {
@@ -13,12 +11,12 @@ public class ServerApp {
         System.out.println("Server started on port 80");
 
         while (true) {
-            Socket localsocket = serverSocket.accept();
-            System.out.println("Accepted connection from " + localsocket.getRemoteSocketAddress());
+            Socket localSocket = serverSocket.accept();
+            System.out.println("Accepted connection from " + localSocket.getRemoteSocketAddress());
 
             new Thread(()->{
                 try {
-                    InputStream is = localsocket.getInputStream();
+                    InputStream is = localSocket.getInputStream();
                     InputStreamReader isr = new InputStreamReader(is);
                     BufferedReader br = new BufferedReader(isr);
 
@@ -35,7 +33,32 @@ public class ServerApp {
                         String value = line.substring(line.indexOf(":") + 1).strip();
                         if (header.equalsIgnoreCase("Host")) host = value;
                     }
-
+                    OutputStream os = localSocket.getOutputStream();
+                    if (!command.equalsIgnoreCase("GET")) {
+                        String httpResponseHead = """
+                                HTTP/1.1 405 Method Not Allowed
+                                Server: Simple-web-server-Buddhi
+                                Date: %s
+                                Content-Type: text/html
+                                
+                                """.formatted(LocalDateTime.now());
+                        os.write(httpResponseHead.getBytes());
+                        os.flush();
+                        String httpResponseBody = """
+                                <!DOCTYPE html>
+                                <html>
+                                <head>
+                                <title>WEB Server | 405 Method Not Allowed</title>
+                                </head>
+                                <body>
+                                    <h1>405 Method Not Allowed </h1>
+                                    <h2>DEP Server does not support %s method.</h2>
+                                </body>
+                                </html>
+                                """.formatted(command);
+                        os.write(httpResponseBody.getBytes());
+                        os.flush();
+                    }
 
 
                 }catch (IOException e) {}
