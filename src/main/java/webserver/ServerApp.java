27ab20a15519989017c5254c6e507c8dc531d6de
Buddhi Ridmal <buddhiridmal@gmail.com>
2025-03-14 12:36:40 +0530

    Open a server socket at port 80 , and accept the connection from it to a local socket

--- src/main/java/webserver/ServerApp.java
+++ src/main/java/webserver/ServerApp.java
@@ -1,4 +1,18 @@
 package webserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(80);
+        System.out.println("Server started on port 80");
+
+        while (true) {
+            Socket localsocket = serverSocket.accept();
+            System.out.println("Accepted connection from " + localsocket.getRemoteSocketAddress());
+
+        }
+    }
 }
